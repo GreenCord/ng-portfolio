f5b18fea079b88c76ce4c21fef258e05de71f5b9
Green Corduroy <steve@nevizen.com>
2021-05-24 20:44:11 -0400

    Remove refs to mongodb

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const compression = require('compression');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
-const ObjectID = mongodb.ObjectID;
+// const mongodb = require('mongodb');
+// const ObjectID = mongodb.ObjectID;
 
 require('dotenv').config();
 
@@ -28,22 +28,22 @@ app.use(express.static(distDir));
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// });
 
 // Routes
 
